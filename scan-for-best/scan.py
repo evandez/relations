bug fix: batched caching the jacobians and biases (caused memory overflow >> corrupted npz files) -- more results with precision @ 5

scan-for-best/scan.py
@@ -15,14 +15,15 @@ from dsets.counterfact import CounterFactDataset
 
 ###########################################################################
 MODEL_NAME = "EleutherAI/gpt-j-6B"  # gpt2-{medium,large,xl} or EleutherAI/gpt-j-6B
-layer = 25
+layer = 15
 consider_residual = False
 approximate_rank = -1
 num_test_cases = 50
+cache_jacobian_batch = 100
 ###########################################################################
 
 print("loading ", MODEL_NAME)
-mt = model_utils.ModelAndTokenizer(MODEL_NAME, low_cpu_mem_usage=False)
+mt = model_utils.ModelAndTokenizer(MODEL_NAME, low_cpu_mem_usage=False, torch_dtype=torch.float16)
 
 model = mt.model
 tokenizer = mt.tokenizer
@@ -33,7 +34,7 @@ print("loaded counterfact dataset")
 
 ###########################################################################
 relation_dct = {
-    'P17'   : {'relation': '{} is located in the country of', 'correct_predict': None, 'cached_JB': None},
+    'P17'   : {'relation': '{} is located in the country of', 'correct_predict': "gpt-j/P17/correct_prediction_P17.json", 'cached_JB': "gpt-j/P17/cached_JB"},
     'P641'  : {'relation': '{} plays the sport of', 'correct_predict': None, 'cached_JB': None},
     'P103'  : {'relation': 'The mother tongue of {} is', 'correct_predict': None, 'cached_JB': None},
     'P176'  : {'relation': '{} is produced by', 'correct_predict': None, 'cached_JB': None},
@@ -41,10 +42,12 @@ relation_dct = {
 ###########################################################################
 
 for relation_id in relation_dct:
-    save_dir = f"gpt-j/{relation_id}/"
+    save_dir = f"gpt-j/{relation_id}"
+    cache_path = f"{save_dir}/cached_JB"
 
     relation = relation_dct[relation_id]["relation"]
     os.makedirs(save_dir, exist_ok=True)
+    os.makedirs(cache_path, exist_ok=True)
 
     print("\n\n")
     print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
@@ -75,7 +78,7 @@ for relation_id in relation_dct:
             if(ok):
                 correct_predict.append(c)
 
-            with open(f"{save_dir}correct_prediction_{relation_id}.json", "w") as f:
+            with open(f"{save_dir}/correct_prediction_{relation_id}.json", "w") as f:
                 json.dump(correct_predict, f)
     else:
         print("Skipped checking, loading from file ==> ", relation_dct[relation_id]['correct_predict'])
@@ -86,82 +89,96 @@ for relation_id in relation_dct:
     print(f"Correctly predicted {len(correct_predict)} targets")
     test_cases = [(c["subject"], -1, c["target_true"]['str']) for c in random.sample(correct_predict, min(num_test_cases, len(correct_predict)))]
 
-    print()
+    print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
     print("Calculating Jacobian weights and biases at all subject tokens")
+    print("-----------------------------------------------------------------------")
     if(relation_dct[relation_id]['cached_JB'] == None):
-        calculated_relation_collections = []
-        for c in tqdm(correct_predict):
-            relation_collection = estimate.estimate_relation_operator__for_all_subject_tokens(
-                model, tokenizer,
-                c["subject"],
-                relation,
-                layer=layer,
-                device=model.device,
-                consider_residual = True,
-                approximate_rank = approximate_rank
-            )
-
-            calculated_relation_collections.append({
-                "subject": c["subject"],
-                "request": c,
-                "all_weights_and_biases": [
-                    {
-                        "weight": calc.weight.cpu().numpy(),
-                        "bias": calc.bias.cpu().numpy(),
-                        "misc": calc.misc
-                    } for calc in relation_collection
-                ]
-            })
-        
-        print("Calculating completed .. saving results")
-        np.savez(
-            f"{save_dir}jacobian_calculations__all_sub_toks__{relation_id}__layer_{layer}.npz", 
-            jacobians = calculated_relation_collections, 
-            allow_pickle = True
-        )
-    else:
-        print("Skipping calculation for Jacobians and Biases, loading from => ", relation_dct[relation_id]['cached_JB'])
-        calculated_relation_collections = np.load(
-            relation_dct[relation_id]['cached_JB'],
-            allow_pickle= True
-        )["jacobians"]
-
-    print("\n")
-
-    with open(f"{save_dir}scan_results_all_sub.txt", "w") as f:
-        for cur_operators in tqdm(calculated_relation_collections):
-            print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz", file=f)
-            print(f'(s = {cur_operators["request"]["subject"]}, r = {relation} [{relation_id}], o = {cur_operators["request"]["target_true"]["str"]})', file=f)
-            print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz", file=f)
-            for calc in cur_operators["all_weights_and_biases"]:
-                print("----------------------------------------------------------------------------------------------------", file=f)
-                print(calc["misc"], file=f)
-                print("----------------------------------------------------------------------------------------------------", file=f)
-
-                is_located_in = estimate.RelationOperator(
-                    model = model, tokenizer= tokenizer,
-                    layer = layer, relation = relation,
-                    weight= torch.tensor(calc['weight'], device = model.device), 
-                    bias= torch.tensor(calc['bias'], device=model.device)
+        for st in range(0, len(correct_predict), cache_jacobian_batch):
+            nd = min(st + cache_jacobian_batch, len(correct_predict))
+            print(f">> calculating from idx => {st} to {nd}")
+            calculated_relation_collections = []
+            for c in tqdm(correct_predict[st:nd]):
+                relation_collection = estimate.estimate_relation_operator__for_all_subject_tokens(
+                    model, tokenizer,
+                    c["subject"], relation,
+                    layer=layer,
+                    device=model.device,
+                    consider_residual = consider_residual,
+                    approximate_rank = approximate_rank
                 )
 
-                tick = 0
-                for subject, subject_token_index, target in test_cases:
-                    objects = is_located_in(
-                        subject,
-                        subject_token_index=subject_token_index,
-                        device=model.device,
-                        return_top_k=5,
-                    )
-                    print(f"{subject}, target: {target}   ==>   predicted: {objects}", file=f)
-                    ok = False
-                    for o in objects:
-                        if target.startswith(o.strip()):
-                            ok = True
-                            break
-                    tick += ok
-                print("----------------------------------------------------------------------------------------------------", file=f)
-                print(f"{tick}/{len(test_cases)}", file=f)
-                print("----------------------------------------------------------------------------------------------------\n", file=f)
+                calculated_relation_collections.append({
+                    "subject": c["subject"],
+                    "request": c,
+                    "all_weights_and_biases": [
+                        {
+                            "weight": calc.weight.cpu().numpy(),
+                            "bias": calc.bias.cpu().numpy(),
+                            "misc": calc.misc
+                        } for calc in relation_collection
+                    ]
+                })
+            print(f"saving results idx => {st} to {nd}")
+            np.savez(
+                f"{cache_path}/jacobian_calculations__all_sub_toks__layer_{layer}___{st}_to_{nd}.npz", 
+                jacobians = calculated_relation_collections, 
+                allow_pickle = True
+            )
+    else:
+        print("Skipping calculation for Jacobians and Biases, loading from => ", relation_dct[relation_id]['cached_JB'])
+        cache_path = relation_dct[relation_id]['cached_JB']
 
-            print("\n", file=f)
+    print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+    print()
+
+    print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+    print("Generating results ... ... ...")
+    print("-----------------------------------------------------------------------")
+    with open(f"{save_dir}/scan_results_all_sub.txt", "w") as f:
+        for cached_jb_file in os.listdir(cache_path):
+            print("loading ... ", cached_jb_file)
+            calculated_relation_collections = np.load(
+                f"{cache_path}/{cached_jb_file}",
+                allow_pickle= True
+            )["jacobians"]
+
+            for cur_operators in tqdm(calculated_relation_collections):
+                print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz", file=f)
+                print(f'(s = {cur_operators["request"]["subject"]}, r = {relation} [{relation_id}], o = {cur_operators["request"]["target_true"]["str"]})', file=f)
+                print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz", file=f)
+                for calc in cur_operators["all_weights_and_biases"]:
+                    print("----------------------------------------------------------------------------------------------------", file=f)
+                    print(calc["misc"], file=f)
+                    print("----------------------------------------------------------------------------------------------------", file=f)
+
+                    is_located_in = estimate.RelationOperator(
+                        model = model, tokenizer= tokenizer,
+                        layer = layer, relation = relation,
+                        weight= torch.tensor(calc['weight'], device = model.device), 
+                        bias= torch.tensor(calc['bias'], device=model.device)
+                    )
+
+                    tick = 0
+                    for subject, subject_token_index, target in test_cases:
+                        objects = is_located_in(
+                            subject,
+                            subject_token_index=subject_token_index,
+                            device=model.device,
+                            return_top_k=5,
+                        )
+                        ok = False
+                        for o in objects:
+                            _o = o.strip()
+                            if(len(_o) == 0):
+                                continue
+                            if target.startswith(_o):
+                                ok = True
+                                break
+                        print(f"{subject}, target: {target}   ==>   predicted: {objects} >>> {ok}", file=f)
+                        tick += ok
+                    print("----------------------------------------------------------------------------------------------------", file=f)
+                    print(f"{tick}/{len(test_cases)}", file=f)
+                    print("----------------------------------------------------------------------------------------------------\n", file=f)
+
+                print("\n", file=f)
+    print("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")