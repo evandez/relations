4c18bdd94d2d53758af5b0d40ed6076e14a2c308
Evan Hernandez <evanfredhernandez@gmail.com>
2023-05-02 22:49:20 -0400

    Change default layer.

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -57,7 +57,7 @@ def main(args: argparse.Namespace) -> None:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--h-layer", type=int, default=5, help="layer to get h from")
+    parser.add_argument("--h-layer", type=int, default=15, help="layer to get h from")
     parser.add_argument("--z-layer", type=int, help="layer to get z from")
     parser.add_argument(
         "--estimator",
