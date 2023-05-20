55db66c490e82f1613801895784cd5fd84116224
Evan Hernandez <evanfredhernandez@gmail.com>
2023-05-19 23:21:52 -0400

    More knowns reporting.

--- scripts/knowns.py
+++ scripts/knowns.py
@@ -19,6 +19,11 @@ def main(args: argparse.Namespace) -> None:
     device = args.device or "cuda" if torch.cuda.is_available() else "cpu"
     mt = models.load_model(args.model, fp16=args.fp16, device=args.device)
     dataset = data.load_dataset_from_args(args)
+
+    kept_relations = 0
+    kept_samples = 0
+    total_relations = 0
+    total_samples = 0
     with torch.device(device):
         filtered = functional.filter_dataset_samples(
             mt=mt, dataset=dataset, n_icl_lm=args.n_icl, n_trials=args.n_trials
@@ -27,9 +32,11 @@ def main(args: argparse.Namespace) -> None:
         relations_by_name = {r.name: r for r in dataset.relations}
         filtered_by_name = {r.name: r for r in filtered.relations}
         for name in relations_by_name:
+            total_relations += 1
             if name not in filtered_by_name:
                 print(f"{REMOVED_MARKER} {name}")
                 continue
+            kept_relations += 1
             print(f"{KEPT_MARKER} {name}")
 
             relation_samples = set(relations_by_name[name].samples)
@@ -37,9 +44,14 @@ def main(args: argparse.Namespace) -> None:
             for sample in relation_samples:
                 if sample in filtered_samples:
                     marker = KEPT_MARKER
+                    kept_samples += 1
                 else:
                     marker = REMOVED_MARKER
+                total_samples += 1
                 print("\t" + f"{marker} {sample}")
+
+    print(f"kept {kept_relations}/{total_relations} relations")
+    print(f"kept {kept_samples}/{total_samples} samples")
 
 
 if __name__ == "__main__":
