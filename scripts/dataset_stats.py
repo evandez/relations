d011c5cde5e80a983d2d32f318d2ab1c40c84a18
Kevin Meng <kevin.meng.01@gmail.com>
2023-05-20 23:50:39 -0400

    More dataset stats

--- scripts/dataset_stats.py
+++ scripts/dataset_stats.py
@@ -1,13 +1,18 @@
 from pathlib import Path
 import json
+import statistics
 
 # Initial Dictionary to store the data
 data_summary = {
     'total_files': 0,
     'total_samples': 0,
     'relations': {},
-    'samples_per_category': {}
+    'samples_per_category': {},
+    'samples_per_relation': {}
 }
+
+# Lists to store samples counts for later statistics calculation
+samples_per_relation_list = []
 
 # Function to update the summary dictionary based on each file's data
 def update_summary(data):
@@ -17,6 +22,11 @@ def update_summary(data):
     if relation not in data_summary['relations']:
         data_summary['relations'][relation] = 0
     data_summary['relations'][relation] += 1
+
+    if relation not in data_summary['samples_per_relation']:
+        data_summary['samples_per_relation'][relation] = 0
+    data_summary['samples_per_relation'][relation] += len(data['samples'])
+    samples_per_relation_list.append(len(data['samples']))
     
     category = data['properties']['domain_name']
     if category not in data_summary['samples_per_category']:
@@ -36,6 +46,14 @@ def process_directory(directory: Path):
 
 # Run the script on a specified directory
 process_directory(Path('./data'))
+
+# Calculate statistics after processing all files
+data_summary['samples_stats'] = {
+    'average_samples_per_relation': statistics.mean(samples_per_relation_list),
+    'min_samples_per_relation': min(samples_per_relation_list),
+    'max_samples_per_relation': max(samples_per_relation_list),
+    'std_dev_samples_per_relation': statistics.stdev(samples_per_relation_list),
+}
 
 # Print the summarized data
 print(json.dumps(data_summary, indent=4))
