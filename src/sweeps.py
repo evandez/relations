3adb30440510f4bb7ef351dc00842b00986fee8a
arnab-api <arnab.api@gmail.com>
2023-05-20 22:34:32 -0400

    using ICL-mean in the faithfulness sweeps

--- src/sweeps.py
+++ src/sweeps.py
@@ -16,8 +16,7 @@ logger = logging.getLogger(__name__)
 
 DEFAULT_RECALL_K = 3
 DEFAULT_N_TRIALS = 3
-DEFAULT_N_TRY_SAMPLES = 3
-DEFAULT_N_ICL_SAMPLES = 5
+DEFAULT_N_TRAIN_SAMPLES = 5
 DEFAULT_BATCH_SIZE = 64
 
 
@@ -28,8 +27,8 @@ class SweepFaithfulnessBetaResults(DataClassJsonMixin):
 
 
 @dataclass(frozen=True)
-class SweepFaithfulnessTrainSampleResults(DataClassJsonMixin):
-    sample: data.RelationSample
+class SweepFaithfulnessTrainResults(DataClassJsonMixin):
+    samples: list[data.RelationSample]
     betas: list[SweepFaithfulnessBetaResults]
 
     def best(self, k: int = 1) -> SweepFaithfulnessBetaResults:
@@ -40,20 +39,19 @@ class SweepFaithfulnessTrainSampleResults(DataClassJsonMixin):
         """Sumarize results in debug logs."""
         best = self.best()
         logger.debug(
-            f"sample={self.sample} | beta={best.beta:.2f} | recall@1={best.recall[0]:.2f}"
+            f"beta={best.beta:.2f} | recall@1={best.recall[0]:.2f} | samples={[str(x) for x in self.samples]}"
         )
 
 
 @dataclass(frozen=True)
 class SweepFaithfulnessLayerResults(DataClassJsonMixin):
     layer: int
-    samples: list[SweepFaithfulnessTrainSampleResults]
+    result: SweepFaithfulnessTrainResults
 
 
 @dataclass(frozen=True)
 class SweepFaithfulnessTrialResults(DataClassJsonMixin):
     prompt_template: str
-    icl_samples: list[data.RelationSample]
     train_samples: list[data.RelationSample]
     layers: list[SweepFaithfulnessLayerResults]
 
@@ -75,15 +73,14 @@ class SweepFaithfulnessRelationResults(DataClassJsonMixin):
         results_by_layer = defaultdict(list)
         for trial in self.trials:
             for layer in trial.layers:
-                for sample in layer.samples:
-                    best = sample.best()
-                    results_by_layer[layer.layer].append(
-                        (
-                            layer.layer,
-                            best.beta,
-                            best.recall[k - 1],
-                        )
+                best = layer.result.best()
+                results_by_layer[layer.layer].append(
+                    (
+                        layer.layer,
+                        best.beta,
+                        best.recall[k - 1],
                     )
+                )
 
         recalls_by_layer = {
             layer: metrics.AggregateMetric.aggregate([x[-1] for x in results])
@@ -130,8 +127,7 @@ def sweep_faithfulness(
     h_layers: Sequence[int] | None = None,
     betas: Sequence[float] | None = None,
     n_trials: int = DEFAULT_N_TRIALS,
-    n_try_samples: int = DEFAULT_N_TRY_SAMPLES,
-    n_icl_samples: int = DEFAULT_N_ICL_SAMPLES,
+    n_train_samples: int = DEFAULT_N_TRAIN_SAMPLES,
     recall_k: int = DEFAULT_RECALL_K,
     batch_size: int = DEFAULT_BATCH_SIZE,
     results_dir: PathLike | None = None,
@@ -168,26 +164,24 @@ def sweep_faithfulness(
         for trial in range(n_trials):
             logger.info(f"begin trial {trial + 1}/{n_trials}")
 
-            if len(relation.samples) <= n_try_samples + n_icl_samples:
+            if len(relation.samples) <= n_train_samples:
                 logger.warning(
                     f"Not enough samples ({len(relation.samples)}) to "
-                    f'test for "{relation.name} since n_try_samples={n_try_samples} and '
-                    f"n_icl_samples={n_icl_samples}. You should fix this by adding more "
-                    "known samples for the relation."
+                    f'test for "{relation.name} with n_train_samples={n_train_samples}.'
+                    f"You should fix this by adding more known samples for the relation."
                 )
                 continue
 
             # Decide which will be the train samples we will try, and which will be the
             # ICL prompt examples.
-            train_relation, _ = relation.split(n_try_samples + n_icl_samples)
+            train_relation, test_relation = relation.split(n_train_samples)
             train_samples = train_relation.samples
-            train_icl_samples = train_samples[:n_icl_samples]
-            train_try_samples = train_samples[
-                n_icl_samples : n_icl_samples + n_try_samples
-            ]
+            train_icl_samples = train_samples[:-1]
 
-            logger.info(f"will do icl using: {[str(x) for x in train_icl_samples]}")
-            logger.info(f"will try: {[x.subject for x in train_try_samples]}")
+            logger.info(f"will train using: {[str(x) for x in train_samples]}")
+            logger.info(
+                f"will do icl for testing using: {[str(x) for x in train_icl_samples]}"
+            )
 
             # Precompute all the hs to speed things up.
             hs_by_subj = _precompute_hs(
@@ -202,63 +196,51 @@ def sweep_faithfulness(
             for h_layer in h_layers:
                 logger.info(f"begin layer: {h_layer}")
 
-                estimator = operators.JacobianIclEstimator(
+                estimator = operators.JacobianIclMeanEstimator(
                     mt=mt, h_layer=h_layer, **kwargs
                 )
 
-                train_sample_results = []
-                for train_sample in train_try_samples:
-                    operator = estimator(
-                        relation.set(
-                            samples=[train_sample, *train_icl_samples],
-                            prompt_templates=[prompt_template],
-                        )
+                operator = estimator(
+                    relation.set(
+                        samples=train_samples,
+                        prompt_templates=[prompt_template],
                     )
-                    assert operator.bias is not None
-                    bias = operator.bias.clone()
+                )
+                assert operator.bias is not None
+                bias = operator.bias.clone()
 
-                    test_samples = [
-                        x
-                        for x in relation.samples
-                        if x != train_sample and x not in train_icl_samples
-                    ]
-                    test_subjects = [x.subject for x in test_samples]
-                    test_hs = [
-                        hs_by_subj[x.subject][h_layer, None] for x in test_samples
-                    ]
-                    test_objects = [x.object for x in test_samples]
+                test_samples = test_relation.samples
+                test_subjects = [x.subject for x in test_samples]
+                test_hs = [hs_by_subj[x.subject][h_layer, None] for x in test_samples]
+                test_objects = [x.object for x in test_samples]
 
-                    results_by_beta = []
-                    recalls_by_beta = []
-                    for beta in betas:
-                        operator.bias[:] = bias * beta
+                results_by_beta = []
+                recalls_by_beta = []
+                for beta in betas:
+                    operator.bias[:] = bias * beta
 
-                        pred_objects = []
-                        for subj, h in zip(test_subjects, test_hs):
-                            preds = operator(subj, h=h, k=recall_k)
-                            pred_objects.append([p.token for p in preds.predictions])
+                    pred_objects = []
+                    for subj, h in zip(test_subjects, test_hs):
+                        preds = operator(subj, h=h, k=recall_k)
+                        pred_objects.append([p.token for p in preds.predictions])
 
-                        recall = metrics.recall(pred_objects, test_objects)
-                        recalls_by_beta.append(recall)
-                        results_by_beta.append(
-                            SweepFaithfulnessBetaResults(beta=beta, recall=recall)
-                        )
-
-                    train_sample_result = SweepFaithfulnessTrainSampleResults(
-                        sample=train_sample, betas=results_by_beta
+                    recall = metrics.recall(pred_objects, test_objects)
+                    recalls_by_beta.append(recall)
+                    results_by_beta.append(
+                        SweepFaithfulnessBetaResults(beta=beta, recall=recall)
                     )
-                    train_sample_result.summarize()
-                    train_sample_results.append(train_sample_result)
+
+                train_result = SweepFaithfulnessTrainResults(
+                    samples=train_samples, betas=results_by_beta
+                )
+                train_result.summarize()
                 layer_results.append(
-                    SweepFaithfulnessLayerResults(
-                        layer=h_layer, samples=train_sample_results
-                    )
+                    SweepFaithfulnessLayerResults(layer=h_layer, result=train_result)
                 )
             trial_results.append(
                 SweepFaithfulnessTrialResults(
                     prompt_template=prompt_template,
-                    icl_samples=train_icl_samples,
-                    train_samples=train_try_samples,
+                    train_samples=train_samples,
                     layers=layer_results,
                 )
             )
