77fc62b0a8f545a3f06e0c5a6a1740f50f60c194
arnab-api <arnab.api@gmail.com>
2023-08-23 13:18:50 -0400

    attribute lens >> cleaned up unused imports/vars, tweaked visualization, added logging

--- src/attributelens/attributelens.py
+++ src/attributelens/attributelens.py
@@ -1,15 +1,11 @@
+import logging
 from collections import defaultdict
 
-import src.attributelens.utils as lens_utils
 from src.functional import compute_hidden_states
 from src.models import ModelAndTokenizer, determine_layer_paths
 from src.operators import LinearRelationOperator
-from src.utils import tokenizer_utils
 
-import numpy as np
-import torch
-from baukit import nethook
-from transformers import AutoModelForCausalLM, AutoTokenizer
+logger = logging.getLogger(__name__)
 
 
 class Attribute_Lens:
@@ -25,11 +21,9 @@ class Attribute_Lens:
         self.layer_output_tmp = layer_output_tmp
 
     def apply_attribute_lens(
-        self,
-        prompt: str,
-        relation_operator: LinearRelationOperator | None
+        self, prompt: str, relation_operator: LinearRelationOperator | None
     ) -> dict:
-        print("prompt: ", prompt)
+        logger.info(f'prompt: "{prompt}"')
 
         if relation_operator is None:
             # Use the identity operator if none is supplied.
@@ -42,24 +36,22 @@ class Attribute_Lens:
                 prompt_template="{}",
             )
 
-        inputs = self.mt.tokenizer(
-            prompt, return_tensors="pt"
-        ).to(self.mt.model.device)
+        inputs = self.mt.tokenizer(prompt, return_tensors="pt").to(self.mt.model.device)
 
-        subject_start, subject_end = 0, inputs['input_ids'].size(1)
+        subject_start, subject_end = 0, inputs["input_ids"].size(1)
         prompt_tokenized = [self.mt.tokenizer.decode(t) for t in inputs.input_ids[0]]
         v_space_reprs: list = []
 
         [hss, out] = compute_hidden_states(
             mt=self.mt, layers=list(range(len(self.layers))), inputs=inputs
         )
-        nextwords = [self.mt.tokenizer.decode(t) for t in out['logits'].max(-1)[1][0]]
+        nextwords = [self.mt.tokenizer.decode(t) for t in out["logits"].max(-1)[1][0]]
 
         for sub_idx in range(subject_start, subject_end):
             v_space_reprs.append(defaultdict(list))
             for layer_idx in range(len(self.layers)):
                 predictions = relation_operator(
-                    subject='', # Not used if h is passed
+                    subject="",  # Not used if h is passed
                     k=self.top_k,
                     h=hss[layer_idx][:, sub_idx],
                 ).predictions
@@ -71,7 +63,7 @@ class Attribute_Lens:
         ret_dict = {}
         ret_dict["prompt_tokenized"] = prompt_tokenized
         ret_dict["v_space_reprs"] = v_space_reprs
-        ret_dict["subject_range"] = (subject_start, subject_end)
+        ret_dict["subject_range"] = (subject_start, subject_end)  # type: ignore
         ret_dict["nextwords"] = nextwords
 
         return ret_dict
